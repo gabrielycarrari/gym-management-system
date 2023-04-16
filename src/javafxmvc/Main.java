35efe481fa4a8749059b1fd62ec89d2def0ff134
Fernanda Oliveira <fernandaoliveira.rms@gmail.com>
2023-04-16 20:00:56 -0300

    update stage: title + icon

--- src/javafxmvc/Main.java
+++ src/javafxmvc/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,7 +19,8 @@ public class Main extends Application {
         scene = new Scene(root);
         
         stage.setScene(scene);
-        stage.setTitle("Sistema de Academia - Login");
+        stage.setTitle("Sistema de Academia");
+        stage.getIcons().add(new Image("src/javafxmvc/images/gym-icon.png"));
         stage.setHeight(720);
         stage.setWidth(1280);
         stage.setResizable(false);
