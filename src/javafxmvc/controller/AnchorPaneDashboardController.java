967dd7d4414d48bd3efc03d20acb8a160110007a
gabrielycarrari <73599857+gabrielycarrari@users.noreply.github.com>
2023-05-11 15:40:25 -0300

    Update AnchorPaneDashboardController.java

--- src/javafxmvc/controller/AnchorPaneDashboardController.java
+++ src/javafxmvc/controller/AnchorPaneDashboardController.java
@@ -1,14 +1,26 @@
 package src.javafxmvc.controller;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 
 public class AnchorPaneDashboardController implements Initializable {
+    @FXML
+    private AnchorPane anchorPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
+        AnchorPane a = new AnchorPane();
+        try {
+            a = (AnchorPane) FXMLLoader.load(getClass().getResource("../view/AnchorPaneGrafico.fxml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        anchorPane.getChildren().setAll(a);
     }
 }
