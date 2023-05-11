0c39b8b239ea5a508d525eebfadc2b636b8ac001
gabrielycarrari <73599857+gabrielycarrari@users.noreply.github.com>
2023-05-11 00:03:32 -0300

    Update CheckOutDAO.java

--- src/javafxmvc/model/dao/CheckOutDAO.java
+++ src/javafxmvc/model/dao/CheckOutDAO.java
@@ -1,4 +1,5 @@
 package src.javafxmvc.model.dao;
+
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -11,39 +12,74 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import src.javafxmvc.model.domain.CheckOut;
+
 public class CheckOutDAO {
+    
     private Connection connection;
+
     public Connection getConnection() {
         return connection;
     }
+
     public void setConnection(Connection connection) {
         this.connection = connection;
     }
+
     public boolean insert(CheckOut checkOut) {
         String sql = "INSERT INTO CheckOut (data, hora, checkIn_id) VALUES (?,?,?)";
         try {
             PreparedStatement stmt = connection.prepareStatement(sql);
             stmt.setDate(1,  java.sql.Date.valueOf(checkOut.getData()));
             stmt.setTime(2,  java.sql.Time.valueOf(checkOut.getHora()));
-            stmt.setInt(3, checkOut.getCheckIn_id());
             stmt.setInt(3, checkOut.getCheckin_id());
             stmt.execute();
             return true;
         } catch (SQLException ex) {
-@@ -46,7 +46,7 @@ public boolean update(CheckOut checkOut) {
+            Logger.getLogger(CheckOutDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+    
+    public boolean update(CheckOut checkOut) {
+        String sql = "UPDATE checkOut SET data=?, hora=?, checkIn_id=? WHERE idCheckOut=?";
+        try {
             PreparedStatement stmt = connection.prepareStatement(sql);
             stmt.setDate(1, java.sql.Date.valueOf(checkOut.getData()));
             stmt.setTime(2, java.sql.Time.valueOf(checkOut.getHora()));
-            stmt.setInt(3, checkOut.getCheckIn_id());
             stmt.setInt(3, checkOut.getCheckin_id());
             stmt.setInt(4, checkOut.getIdCheckOut());
             stmt.execute();
             return true;
-@@ -80,7 +80,7 @@ public List<CheckOut> list() {
+        } catch (SQLException ex) {
+            Logger.getLogger(CheckOutDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public boolean delete(CheckOut checkOut) {
+        String sql = "DELETE FROM checkOut WHERE idCheckOut=?";
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            stmt.setInt(1, checkOut.getIdCheckOut());
+            stmt.execute();
+            return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(CheckOutDAO.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    }
+
+    public List<CheckOut> list() {
+        String sql = "SELECT * FROM checkOut";
+        List<CheckOut> retorno = new ArrayList<>();
+        try {
+            PreparedStatement stmt = connection.prepareStatement(sql);
+            ResultSet resultado = stmt.executeQuery();
+            while (resultado.next()) {
+                CheckOut checkOut = new CheckOut();               
                 checkOut.setIdCheckOut(resultado.getInt("idCheckOut"));
                 checkOut.setData(resultado.getDate("data").toLocalDate());
                 checkOut.setHora(resultado.getTime("hora").toLocalTime());
-                checkOut.setCheckIn_id(resultado.getInt("checkIn_id"));
                 checkOut.setCheckin_id(resultado.getInt("checkIn_id"));
                 retorno.add(checkOut);
             }
@@ -52,9 +88,11 @@ public class CheckOutDAO {
         }
         return retorno;
     }
+
     public boolean search(int checkIn_id) { 
         String sql = "SELECT idCheckOut FROM CheckOut WHERE checkIn_id=?";
         boolean retorno = false;
+
         try {
             PreparedStatement stmt = connection.prepareStatement(sql);
             stmt.setInt(1, checkIn_id);
